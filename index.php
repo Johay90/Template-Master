c80c3cd60d4a86da45871ef3fc7011a1d4becb95
Rapid-Dev <johay.jjj@googlemail.com>
2015-05-20 04:27:39 +0100

    Update Design
    Draft design of Template Master complete including nav bar, background,
    layout and containers.

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<body>
+  <head>
+    <title>Template master - by Jonathan Mummery</title>
+    <link rel="Stylesheet" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700italic,300,400' rel='stylesheet' type='text/css'>
+  </head>
+
+  <div id="navbar">
+    <ul>
+      <li><a class="navbar" href="#">Home</a></li>
+      <li><a class="navbar" href="#">Templates</a></li>
+      <li><a class="navbar" href="#">Upload</a></li>
+    </ul>
+  </div>
+
+  <div id="wrapper">
+
+    <div id="content-left">
+      <h1>About Template Master</h1>
+      <p>Lorem ipsum dolor sit amet, ne magna definiebas qui, unum decore dolorum quo at, id per autem hendrerit. Saepe utamur nusquam vix ne. Novum scripta ex eam. Ad sed alia corrumpit assentior. Ad lorem veniam qui, sonet volutpat explicari ex nec.</p><br />
+
+      <p>Cu duo democritum mediocritatem, mucius labores ius cu. Et per appetere singulis sensibus. Erant molestie no ius. Id qui quod copiosae dignissim, audiam impetus democritum te sed.</p><br />
+
+      <p>Sed an tota velit intellegat, mei postulant petentium persecuti at. Et sed option volumus, ei nusquam legendos mei. Usu nonumy altera te, facer inani invenire quo te. At usu suas autem erant, vix nemore fabulas id. Soluta labitur ceteros ut nam, denique persequeris eu his.</p><br />
+
+      <p>Eu eum porro similique, pro an aperiam facilisis, maiorum vituperata ei sit. Dignissim definitiones has no. Ex his utamur alterum dolores. Harum noster inimicus has no, te tale iisque mea. In noster principes vel, nec id alii ferri nostrum.</p><br />
+
+      <p>Velit quidam scripserit vel no. No lorem oportere reprehendunt vel, an eam consetetur efficiantur, idque cotidieque ea usu. Ad quo splendide expetendis. Vim mundi deleniti similique in.</p>
+
+    </div>
+
+    <div id="content-right">
+
+    </div>
+
+    <div id="footer">
+
+    </div>
+
+  </div>
