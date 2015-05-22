6a6790efd55ae8d83db5ca787b61efc3f743610b
Rapid-Dev <johay.jjj@googlemail.com>
2015-05-22 08:36:03 +0100

    (example design) download template page complete

---
+++ example_template.php
@@ -0,0 +1,58 @@
+<html>
+<body>
+  <head>
+    <title>Template master - by Jonathan Mummery</title>
+    <link rel="Stylesheet" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700italic,300,400' rel='stylesheet' type='text/css'>
+  </head>
+
+  <div id="navbar">
+    <ul>
+      <li><a class="navbar" href="index.php">Home</a></li>
+      <li><a class="navbar" href="template.php">Templates</a></li>
+      <li><a class="navbar" href="#">Upload</a></li>
+    </ul>
+  </div>
+
+  <div id="wrapper">
+
+    <div id="content-left">
+      <!-- title, thumbnail, Description, tags -->
+      <h1>[Green/Grey] Simple HTML and CSS layout</h1>
+      <p>This design is a quick (ugly) mockup HTML and CSS design. It uses 4 div tags, a wrapper, nav bar, banner and the content. This design while not the best looking was made in mind of somebody wanting to edit the design, ie. It was made with ease of editting in mind. Most design changes can easily be made within the CSS without the need to edit any of the main content (html).</p>
+
+      <p>This design is perfect for a newcomer to HTML and CSS whos wanting to test their knowledge and skills and start editting some code to make a nicer looking website.</p><br /><br />
+
+      <a href="http://i.gyazo.com/5f8758a5f2e9f928be8d296e8b05198b.png"><img align="center" src="http://www.makeathumbnail.com/thumbnails/image367000.png" alt="Thumbnail provided by MakeAThumbnail.com" /></a><br /><br />
+
+      <a align="center" class="template" href="#">Download Now!</a>
+
+    </div>
+
+    <div id="content-right">
+      <h2>Newest uploads on Template Master</h2>
+        <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+        <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+        <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+        <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+
+      <h2>Popular on Template Master</h2>
+      <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+      <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+      <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+      <a class="sidebar" href="#"> Design testing one testing one 1</a><br />
+
+    </div>
+
+  </div>
+
+  <div id="footer">
+    <a class="footer" href="#"> About</a>
+    <a class="footer" href="#"> Contact</a>
+    <a class="footer" href="#"> Terms of Use</a>
+    <a class="footer" href="#"> Privacy</a>
+    <a class="footer" href="#"> Like Us</a>
+    <a class="footer" href="#"> Follow us</a>
+    <p class="footer">Copyright © 2015 Jonathan Mummery. All Rights Reserved.</p>
+  </div>
